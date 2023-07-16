d17e6c0d30198da920f30477c6b9184c615071e6
Welkinoid52 <71551574+Welkinoid52@users.noreply.github.com>
2023-07-16 22:50:23 +0530

    Create sample.sql

---
+++ sample.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA StudentsInfo;
+CREATE TABLE Students
+(
+StudentID int,
+StudentName varchar(255),
+ParentName varchar(255),
+Address varchar(255),
+PostalCode int,
+City varchar(255)
+);
+
